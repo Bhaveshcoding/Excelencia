d280d0e556cbc558855b3a3de28c31e7ab5a7d06
Smarg1 <105309480+Smarg1@users.noreply.github.com>
2024-10-17 19:32:50 +0530

    Added 5 Servo Support

--- excelencia_template.ino
+++ excelencia_template.ino
@@ -2,13 +2,20 @@
 #include <Servo.h>
 #define DELAY_MIL 2.5
 
-Servo claw_pin;
-Servo Servo1;
+Servo claw_pin; // Claw
+Servo Servo1; // Top
+Servo Servo2; // Middle
+Servo Servo3; // Bottom
+Servo Servo4; // Rotater
 
 // Functions
 void setup() {
   Serial.begin(2400);
-  addServo(Servo1, 2); addServo(claw_pin, 3);
+  addServo(claw_pin, 2);
+  addServo(Servo1, 3);
+  addServo(Servo2, 4);
+  addServo(Servo3, 5);
+  addServo(Servo4, 6);
   Serial.println("Starting ...");
   perform();
 }
